8a7c2b554dea74fe9e764498f0c87deda3314af6
Edgar0bsj <edgarbsj.dev@gmail.com>
2025-10-07 13:59:36 -0300

    :sparkles: feat: Página home && Layout base

--- adminx-web/app/page.tsx
+++ adminx-web/app/page.tsx
@@ -1,9 +1,23 @@
 "use client";
+import LayoutPages from "@/components/LayoutPages";
 
 export default function Home() {
   return (
     <>
-      <section className="container mx-auto">Home Page</section>
+      <LayoutPages>
+        {/* CONTEÚDO PRINCIPAL */}
+        <main className="flex-grow">
+          <div className="container mx-auto p-4">
+            <h2 className="text-2xl font-semibold mb-4">Bem-vindo!</h2>
+            <p className="text-gray-700 leading-relaxed">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+              vestibulum erat, nec ultricies lorem. Sed efficitur, tortor ac
+              ultrices luctus, nisl est ultrices metus, non ultricies eros nulla
+              nec turpis.
+            </p>
+          </div>
+        </main>
+      </LayoutPages>
     </>
   );
 }
---
+++ adminx-web/components/LayoutPages.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function LayoutPages(props: any) {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        {/* NAVBAR */}
+        <header className="p-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-[30px] italic font-bold">Admin X</h1>
+            <nav className="space-x-1 ">
+              <a href="#">
+                <Button className="bg-cyan-500">Home</Button>
+              </a>
+              <a href="#">
+                <Button className="bg-cyan-500">Perfil</Button>
+              </a>
+              <a href="#">
+                <Button className="bg-cyan-500">Sair</Button>
+              </a>
+            </nav>
+          </div>
+        </header>
+        <hr />
+
+        {/* CONTEÚDO PRINCIPAL */}
+        {props.children}
+
+        {/* FOOTER */}
+        <footer className="bg-gray-800 text-white text-center p-4">
+          <div className="container mx-auto">
+            <p>
+              © 2025 Criado por <strong>Edgar Junior</strong>.
+            </p>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
