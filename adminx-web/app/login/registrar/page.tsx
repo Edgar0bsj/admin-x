101ea369bf988943d3218530de9f289ad03a7666
Edgar0bsj <edgarbsj.dev@gmail.com>
2025-10-07 13:20:39 -0300

    :recycle: refactor: Refatorando a pagina de Registro

--- adminx-web/app/login/registrar/page.tsx
+++ adminx-web/app/login/registrar/page.tsx
@@ -1,9 +1,58 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Registrar() {
   return (
     <>
-      <section>Registrar page</section>
+      <div className="min-h-screen flex items-center justify-center p-4">
+        <div className="w-full max-w-sm shadow-md rounded-lg p-6 sm:p-8 md:max-w-md">
+          <h1 className="text-6xl font-semibold text-center mb-6 font-bold italic">
+            Registrar
+          </h1>
+
+          <form className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium mb-1">Nome</label>
+              <Input
+                type="text"
+                placeholder="Digite seu nome"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium mb-1">Email</label>
+              <Input
+                type="email"
+                placeholder="Digite seu Email"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium mb-1">Senha</label>
+              <Input
+                type="password"
+                placeholder="Digite sua senha"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium mb-1">
+                Confirmar Senha
+              </label>
+              <Input
+                type="password"
+                placeholder="Digite sua senha"
+                className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <Button type="submit" className="w-full py-4 mt-5">
+              Registrar
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
